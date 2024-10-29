49dd9593cead1799683780aaa429d4d76ab293c0
Aryamann Chhabra <aryamann.c04@gmail.com>
2024-10-28 20:32:18 -0400

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert( 3 + 3) // we are using NodeJS not a browser
+
+console.log(3 + 3);
+console.log("Aryamann");
+
+let name = "Aryamann"
+let age = 20
+let isLogginIn = false
+let state;
+
+// numbers ==> 2 to pwer 53
+// bigint
+// string => ""
+//boolean ==> true/false
+// null ==> standAlone value (also is an object)
+// undefined => 
+// symbol = unique 
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
